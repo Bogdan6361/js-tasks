afdc6202ddb659d07c6690644e57e728dc24a51d
Bogdan6361 <74094803+Bogdan6361@users.noreply.github.com>
2020-12-06 11:30:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,89 @@
+
+
+
+let x;
+let y,z;
+x=99;
+y=5;
+z=true;
+let user="vasya";
+console.log(user);
+console.log(x);
+x='world';
+console.log(x);
+x='masha';
+console.log(x);
+x=100;
+console.log(x+y);
+let sum=0;
+sum=x+y;
+console.log(sum);
+let _ = 100;
+let $ = 10;
+let result = _ + $;
+console.log(result);
+
+let x1 = 100,
+x2 = 200;
+console.log('x1=', x1, 'x2=', x2);
+x1 = x2;
+console.log('x1=', x1, 'x2=', x2);
+let x3='uuu';
+x1=x3;
+console.log('x1=', x1, 'x2=', x2, 'x3=', x3);
+console.log(`x1=${x1}`);
+const MYCOLOR = "GREY";
+const PI = 3.1415;
+const user_age = 27;
+
+console.log(MYCOLOR);
+console.log(user_age);
+
+let user_name;
+user_name = prompt('enter your name');
+alert(`Hello, ${user_name}!`);
+console.log(`Hello, ${user_name}!`);
+
+
+/*let chislo;
+chislo = prompt('enter tsyfyr');
+console.log(chislo); */
+
+let z1,z2,res;
+z1=100;
+z2=2;
+res=z1+z2;
+console.log(res);
+res=z1-z2;
+console.log(res);
+res=z1*z2;
+console.log(res);
+res=z1/z2;
+console.log(res);
+res=z1+5;
+console.log('res=',res);
+res=res+5;
+console.log('res=',res);
+
+let chislo;
+chislo = prompt('enter tsyfyr');
+console.log(chislo*chislo);
+
+let ad = 17, ab=15;
+
+let rez=(ad+ab) / 2;
+console.log(rez);
+
+let h=5;
+rem=h*h;
+console.log(rem);
+
+let s='hello user';
+let d=10;
+let f=3.14154
+let g=true;
+console.log(s,d,f,g);
+let j=prompt('vvedi summu prodazh');
+rea=j*1.1;
+console.log(rea);
+
---
+++ task61220.js
@@ -0,0 +1,81 @@
+//2
+let x=5;
+let res=x*30-100;
+console.log(res);
+//3
+let y, z, rea;
+y=3;
+z=4;
+rea=y*z;
+console.log('Ploschad pryamougolnika=', rea);
+//5
+const current_year = 2020;
+const last_year = current_year-1;
+const next_year=urrent_year+1;
+console.log('last_year', last_year);
+console.log('next_year', next_year);
+//7
+let a, b, c;
+a=100;
+b=45;
+c=145;
+c=a*x+b;
+x=(c-b)/a;
+console.log('x=',x);
+//8
+let hours;
+hours=parseInt(prompt('enter some hour'));
+let minuts=hours*60;
+let seconds=minuts*60;
+console.log('h=', hours, 'm=', minuts, 's=', seconds);
+//9
+const PI = 3.14;
+let r, v, s;
+r=parseFloat(prompt('r='));
+v=(3/4) * PI *r ** 3;
+s = 4 * PI * r ** 2;
+console.log('v=', v, 's=', s);
+//10
+let N, M, x, y;
+x = 10;
+y = 15;
+N=parseInt(prompt('vvedi skolko tetradey'));
+M=parseInt(prompt('vvedi skolko ruchek'));
+let total = N*x+M*y;
+console.log(total);
+//14
+let answer_user, answer_comp;
+answer_user = parseInt(prompt('2+2=?'));
+answer_comp = 4;
+if(answer_user == answer_comp){
+    alert("You're right!");
+
+} else{
+    alert('Bad answer:(');
+}
+//15
+
+let a,b;
+let answer_user, answer_comp;
+a = prompt('vvedi pervoye chislo');
+b = prompt('vvedi vtoroye chislo');
+answer_user  = prompt('chemu ravno ih proizvedenie');
+answer_comp = a*b;
+if(answer_user == answer_comp){
+    alert(`You're right! It's ${answer_comp}`);
+} else{
+    alert(`Bad answer:( It's ${answer_comp}`);
+}
+
+//4
+
+let sum, countM, pr;
+sum=1000;
+countM=18;
+pr=0.05;
+pr /=100;
+// sum=sum+sum*pr;
+// sum=sum*(1+pr);
+sum *= (1 + pr) * countM;
+console.log(sum);
+//6, 11, 12, 13, 16 - cam.
